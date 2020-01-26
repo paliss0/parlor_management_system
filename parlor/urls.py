f808c1533100954c26fa6c28d3759bf203b7eb5c
sujan079 <sujantimalsina079@gmail.com>
2020-01-26 23:41:04 +0545

    "Some Few Initial Setup"

---
+++ parlor/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns=[
+    path('',get_parlor_home),
+    path('add-parlor/',get_add_parlor),
+    path('update-parlor/<int:ID>/',get_update_parlor)
+]
--- parlor/views.py
+++ parlor/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+
+
+def get_parlor_home(req):
+    return render(req,'parlor_home.html')
+
+def get_add_parlor(req):
+    context={
+        "page_title":"Add Parlor",
+        "add_new_parlor":True
+    }
+
+    return render(req,'add_update_parlor.html',context=context)
+
+
+def get_update_parlor(req,ID):
+    context={
+        "page_title":"Update Parlor"
+    }
+
+    return render(req,"add_update_parlor.html",context=context)
+
