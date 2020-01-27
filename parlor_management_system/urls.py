747a6f64f45edee6b2ba641d62c2f26acfaa2760
sujan079 <sujantimalsina079@gmail.com>
2020-01-28 05:23:22 +0545

    "User Auth Complete"

--- parlor_management_system/urls.py
+++ parlor_management_system/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('parlor/',include('parlor.urls'))
+    path('parlor/',include('parlor.urls')),
+    path('user/',include('user.urls'))
 ]
 
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth.models import User
 # Create your views here.
+
+def user_login(req):
+    if req.method=="GET":
+        return render(req,'login.html')
+    else:
+        username=req.POST['username']
+        password=req.POST['password']
+
+        user=authenticate(username=username,password=password)
+
+        if user is not None:
+            return redirect('parlor_home')
+        else:
+            return redirect('login')
+
+def sign_up(req):
+    if req.method=="GET":
+        return render(req,'sign-up.html')
+    else:
+        username=req.POST['username']
+        password=req.POST['password']
+        email=req.POST['email']
+
+        user=User.objects.create_user(username=username,email=email,password=password)
+        user.save()
+
+        return redirect('login')
+
+def user_logout(req):
+    logout(req)
+    redirect ('login')
+    
