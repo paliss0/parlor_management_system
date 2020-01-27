a188c229a879fd7fecd8db688e8e64d15cda61f4
sujan079 <sujantimalsina079@gmail.com>
2020-01-27 19:28:55 +0545

    "Crud Complete"

--- parlor/urls.py
+++ parlor/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns=[
     path('',get_parlor_home,name="parlor_home"),
     path('add-parlor/',get_add_parlor),
-    path('update-parlor/<int:ID>/',get_update_parlor)
+    path('update-parlor/<int:ID>/',get_update_parlor),
+    path('delete-parlor/<int:ID>/',delete_parlor)
 ]
--- parlor/views.py
+++ parlor/views.py
@@ -3,13 +3,25 @@ from .models import Parlor
 
 
 def get_parlor_home(req):
-    return render(req,'parlor_home.html')
+
+    all_parlor=Parlor.objects.all()
+
+    if 'search_query' in req.GET:
+        search_query=req.GET['search_query']
+        if search_query == "":
+            all_parlor=Parlor.objects.all()
+        else:
+            all_parlor=Parlor.objects.filter(parlor_name=search_query)
+    
+    context={
+        "parlors":all_parlor
+    }
+    return render(req,'parlor_home.html',context=context)
 
 def get_add_parlor(req):
     if req.method=="GET":
         context={
-            "page_title":"Add Parlor",
-            "add_new_parlor":True
+            "page_title":"Add Parlor"
         }
 
         return render(req,'add_update_parlor.html',context=context)
@@ -29,9 +41,34 @@ def get_add_parlor(req):
 
 
 def get_update_parlor(req,ID):
-    context={
-        "page_title":"Update Parlor"
-    }
+    current_parlor=Parlor.objects.get(id=ID)
 
-    return render(req,"add_update_parlor.html",context=context)
+    if req.method=="GET":
+        context={
+            "page_title":"Update Parlor",
+            "update_parlor":True,
+            "parlor":current_parlor
 
+        }
+
+        return render(req,"add_update_parlor.html",context=context)
+    else:
+        parlor_name=req.POST['parlor_name']
+        parlor_desc=req.POST['parlor_description']
+        address=req.POST['address']
+        price=req.POST['price']
+
+        current_parlor.parlor_name=parlor_name
+        current_parlor.parlor_description=parlor_desc
+        current_parlor.address=address
+        current_parlor.price=price
+
+        current_parlor.save()
+
+        return redirect('parlor_home')
+
+
+def delete_parlor(req,ID):
+    current_parlor=Parlor.objects.get(id=ID)
+    current_parlor.delete()
+    return redirect("parlor_home")
