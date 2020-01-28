63d4a4466aecd9dfe1f1d401855111c48d14551a
sujan079 <sujantimalsina079@gmail.com>
2020-01-28 21:02:23 +0545

    "Test Case"

--- parlor/models.py
+++ parlor/models.py
@@ -2,23 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Parlor(models.Model):
-    parlor_logo=models.ImageField()
+    parlor_logo=models.ImageField(null=True)
     parlor_name=models.TextField()
     parlor_description=models.TextField()
     address=models.TextField()
     price=models.IntegerField()
+
+    def valid_price(self):
+        return self.price>0
+    
+    def valid_name(self):
+        return self.parlor_name==""
 
 class ParlorUser(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     address=models.TextField()
     phone=models.IntegerField()
 
+    def valid_phone(self):
+        return self.phone>0
+
 
 class ParlorEmployee(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     address=models.TextField()
     phone=models.IntegerField()
-    available_time=models.IntegerField()
+    available_time=models.TextField()
+
+    def count_appointments(self):
+        return self.appointment_of.all().cout()
 
 class Appointment(models.Model):
     appointment_of=models.ManyToManyField(to=ParlorUser)
--- parlor/tests.py
+++ parlor/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from .models import Parlor
 # Create your tests here.
