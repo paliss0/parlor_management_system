e1921a27deeeb120145b1a389a15c6e319b2b887
sujan079 <sujantimalsina079@gmail.com>
2020-01-28 09:45:25 +0545

    Model Complete

--- parlor/models.py
+++ parlor/models.py
@@ -1,14 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-
-#Models are for database. Parlor is table and content inside are column names in sql database
 class Parlor(models.Model):
     parlor_logo=models.ImageField()
     parlor_name=models.TextField()
     parlor_description=models.TextField()
     address=models.TextField()
     price=models.IntegerField()
+
+class ParlorUser(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address=models.TextField()
+    phone=models.IntegerField()
+
+
+class ParlorEmployee(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address=models.TextField()
+    phone=models.IntegerField()
+    available_time=models.IntegerField()
+
+class Appointment(models.Model):
+    appointment_of=models.ManyToManyField(to=ParlorUser)
+    appointment_for=models.ManyToManyField(to=ParlorEmployee)
+    date=models.DateField(auto_now=True)
+
+
+
+
+
+
 
 
 
