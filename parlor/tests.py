75c3cade332a50e5e87555657b65a457bc36f235
sujan079 <sujantimalsina079@gmail.com>
2020-01-29 09:23:18 +0545

    "Test"

--- parlor/tests.py
+++ parlor/tests.py
@@ -23,9 +23,16 @@ class ModelTestCase(TestCase):
     def test_count_appointments(self):
         user=User.objects.create_user(username="ABC",email="abc@gmail.com",password="123456789")
         emp_user=User.objects.create_user(username="ABCD",email="abc@gmail.com",password="123456789")
+
+        user.save()
+        emp_user.save()
         
         parlor_emp=ParlorEmployee(user=emp_user,address="ktm",phone=98605241,available_time="9-5")
         parlor_user=ParlorUser(user=user,address="ktm",phone=986015632)
+
+        parlor_emp.save()
+        parlor_user.save()
+
 
         appointment=Appointment(id=1)
 
